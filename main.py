7a4d83558d63bbff13170b42ab7c4cdff43a1791
Henrique-TO <144277732+Henrique-TO@users.noreply.github.com>
2023-09-13 16:52:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+numero_1 = 200
+numero_2 = 352
+meu_nome = input('insira seu nome ')
+Comprimento = input("insira um comprimento")
+#1  -  CRIE UM SISTEMA QUE SOME 2  NUMEROS
+print (numero_1 + numero_2)
+
+#2 - CRIE UM SISTEMA QUE IMPRIMA SEU NOME NA TELA
+print (meu_nome)
+
+#3 - CRIE UM SISTEMA QUE IMPRIMA SEU NOME E UM CUMPRIMENTO APÓS nome(CONCATENE)
+print (meu_nome + Comprimento)
+
+#4 - CRIE UM SISTEMA QUE MULTIPLIQUE 2 NÚMEROS
+print (numero_1 * numero_2)
+
+#5 - CRIE UM SISTEMA QUE SUBTRAIA 2 NÚMEROS
+print (numero_1 - numero_2)
+
+#6 - CRIE UM SISTEMA QUE DIVIDA 2 NÚMEROS
+print (numero_1 / numero_2)
+
+
+
+
